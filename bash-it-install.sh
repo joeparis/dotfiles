5bbc9d3d5d85f3d2f2deb71524d05a643058e8eb
Joe Paris <joe.paris@outlook.com>
2019-09-13 14:55:36 -0700

    Replaced "source" with "." to be POSIX compliant.

--- bash-it-install.sh
+++ bash-it-install.sh
@@ -10,9 +10,9 @@ echo -e '\n\n##bash-it\n# Path to the bash-it configuration\nexport BASH_IT="$HO
 echo -e '\n# Lock and Load a custom bash-it theme file location /.bash_it/themes/\nexport BASH_IT_THEME="joe1"\n' >> $HOME/.bashrc
 echo -e "\n# Don't check mail when opening terminal.\nunset MAILCHECK" >> $HOME/.bashrc
 echo -e '\n# Set this to false to turn off version control status checking within the prompt for all themes\nexport SCM_CHECK=true' >> $HOME/.bashrc
-echo 'source "$BASH_IT"/bash_it.sh' >> $HOME/.bashrc
+echo '. "$BASH_IT"/bash_it.sh' >> $HOME/.bashrc
 
-source $HOME/.bashrc
+. $HOME/.bashrc
 
 bash-it enable alias apt fuck
 bash-it enable plugin explain less-pretty-cat tmux
