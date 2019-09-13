5342ccdcd68712e7fea26bf8b11743c055b98d2d
Joe Paris <joe.paris@outlook.com>
2019-09-13 15:47:39 -0700

    Switched from wget to curl.

--- base-install.sh
+++ base-install.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+if [ ! -x /usr/bin/curl ]
+then
+        sudo apt install curl
+fi
+
 # .bashrc
 if [ -f $HOME/.bashrc ]; then
     rm $HOME/.bashrc
 fi
-wget -o $HOME/.bashrc https://raw.githubusercontent.com/joeparis/dotfiles/master/base_bashrc
+curl -L -o $HOME/.bashrc https://raw.githubusercontent.com/joeparis/dotfiles/master/base_bashrc
 chmod 644 $HOME/.bashrc
 
 # .bash_aliases
 if [ -f $HOME/.bash_aliases ]; then
     rm $HOME/.bash_aliases
 fi
-wget -o $HOME/.bash_aliases https://raw.githubusercontent.com/joeparis/dotfiles/master/bash_aliases
+curl -L -o $HOME/.bash_aliases https://raw.githubusercontent.com/joeparis/dotfiles/master/bash_aliases
 chmod 644 $HOME/.bash_aliases
 
 # .dircolors
 if [ -f $HOME/.dircolors ]; then
     rm $HOME/.dircolors
 fi
-wget -o $HOME/.dircolors https://raw.githubusercontent.com/joeparis/dotfiles/master/dircolors
+curl -L -o $HOME/.dircolors https://raw.githubusercontent.com/joeparis/dotfiles/master/dircolors
 chmod 644 $HOME/.dircolors
 
 # .groot-lecture
 mkdir -p $HOME/.lectures/
-wget -o $HOME/.lectures/.groot-lecture https://raw.githubusercontent.com/joeparis/dotfiles/master/groot-lecture
+curl -L -o $HOME/.lectures/.groot-lecture https://raw.githubusercontent.com/joeparis/dotfiles/master/groot-lecture
 chmod 644 $HOME/.lectures/.groot-lecture
 touch $HOME/lecture
 echo -e "Defaults\tlecture = always" >> $HOME/lecture
