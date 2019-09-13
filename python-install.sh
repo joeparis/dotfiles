5510697205a0041d6ccd6768e28ff4e7aad5f005
Joe Paris <joe.paris@outlook.com>
2019-09-13 14:09:37 -0700

    Added more Python tooling.

--- python-install.sh
+++ python-install.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo -H python3 get-pip.py
+yes | rm get-pip.py
+pip3 install --user pipx
+pipx install black
+pipx install pipenv
+pipx install pytest
+pipx install pylint
+pipx install pydocstyle
+pipx install pycodestyle
+
 echo -e '\n\n## Python pipx\neval "$(register-python-argcomplete pipx)"' >> $HOME/.bashrc
