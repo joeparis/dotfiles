925c69a5aa9e2739634959bc6f81324e02b92366
Joe Paris <joe.paris@outlook.com>
2019-09-14 01:12:40 -0700

    Added fonts.

--- cli-tooling-install.sh
+++ cli-tooling-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo | sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt install aptitude autoconf automake build-essential build-essential cowsay curl fortune gcc-9 git glances htop httpie imagemagick lolcat most python-argcomplete python3.6-dev python3.6-distutils python3.6-doc python3.6-tk python3.6-venv tmux toilet xclip youtube-dl -y
+sudo apt install aptitude autoconf automake build-essential build-essential cowsay curl fonts-font-awesome fonts-hack-ttf fonts-firacode fortune gcc-9 git glances htop httpie imagemagick lolcat most python-argcomplete python3.6-dev python3.6-distutils python3.6-doc python3.6-tk python3.6-venv tmux toilet xclip youtube-dl -y
 echo | sudo add-apt-repository --remove ppa:ubuntu-toolchain-r/test
