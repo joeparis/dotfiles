d592d22b6a8f72c6212568cbae374d41ea855bc6
Joe Paris <joe.paris@outlook.com>
2019-09-14 00:29:56 -0700

    Added todo list.

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+set -e # exit immediately on error
+
 curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/base-install.sh | bash
 curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/cli-tooling-install.sh | bash
 curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/python-install.sh | bash
 curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/bash-it-install.sh | bash
-curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/wsl-install.sh | bash
+
+# Test if runing in WSL (brittle)
+if grep -qE "(Microsoft@Microsoft.com)" /proc/version &> /dev/null ; then
+    curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/wsl-install.sh | bash
+fi
+
 curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/wrap-up-install.sh | bash
+
+# TODO:
+# Add vim config
+# Add GUI application install/config for full-blown Linux environments
+# Add Ubuntu/Manjaro detection
