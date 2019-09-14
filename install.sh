72535ff8eb689ce2916cd21e6a7b227218d1ac21
Joe Paris <joe.paris@outlook.com>
2019-09-13 23:21:17 -0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/base-install.sh | bash
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/cli-tooling-install.sh | bash
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/python-install.sh | bash
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/bash-it-install.sh | bash
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/wsl-install.sh | bash
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/wrap-up-install.sh | bash
