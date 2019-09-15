242c7ec9c8dfecc23850dc50ecdb931111fa9ae7
Joe Paris <joe.paris@outlook.com>
2019-09-15 16:41:27 -0700

    Initial commit.

---
+++ nano-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Install nano 4.4 and configure to my liking.
+
+# pre-req
+sudo apt install libncursesw5-dev -y
+
+curl -L https://www.nano-editor.org/dist/v4/nano-4.4.tar.gz -o $HOME/nano-4.4.tar.gz
+tar -xzf nano-4.4.tar.gz
+
+$HOME/nano-4.4/./configure --prefix=/usr --enable-all
+$HOME/nano-4.4/make
+sudo $HOME/nano-4.4/make install
+
+sudo rm -rf nano-4.4
+rm nano-4.4.tar.gz
+
+curl -L https://raw.githubusercontent.com/joeparis/dotfiles/master/nanorc -o $HOME/.nanorc
+
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
