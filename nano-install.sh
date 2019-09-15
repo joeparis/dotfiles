e8f7868d509c82cc959c9e6ee65c308c0c5bb62c
Joe Paris <joe.paris@outlook.com>
2019-09-15 16:51:43 -0700

    Set noninteractive install of pre-reqs.

--- nano-install.sh
+++ nano-install.sh
@@ -2,7 +2,8 @@
 # Install nano 4.4 and configure to my liking.
 
 # pre-req
-sudo apt install libncursesw5-dev -y
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install libncursesw5-dev -y
 
 curl -L https://www.nano-editor.org/dist/v4/nano-4.4.tar.gz -o $HOME/nano-4.4.tar.gz
 tar -xzf nano-4.4.tar.gz
