2a63199dc185dea74b0150dc1d58768515a6a622
Joe Paris <joe.paris@outlook.com>
2019-09-15 18:34:19 -0700

    Fixed typo.

--- nano-install.sh
+++ nano-install.sh
@@ -9,7 +9,7 @@ curl -L https://www.nano-editor.org/dist/v4/nano-4.4.tar.gz -o $HOME/nano-4.4.ta
 tar -xzf nano-4.4.tar.gz
 
 cd $HOME/nano-4.4
-.cconfigure --prefix=/usr --enable-all
+.configure --prefix=/usr --enable-all
 make
 sudo make install
 
