2e9dcc8519e6bb4d1ee90323f35dafda998a9d1e
Joe Paris <joe.paris@outlook.com>
2019-10-01 19:52:41 -0700

    Update wsl-install.sh

--- wsl-install.sh
+++ wsl-install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -e "\n## WSL\n# Forward opengl calls to Windows.\nexport LIBGL_ALWAYS_INDIRECT=1\n# Forward X11 GUIS to Windows.\nexport DISPLAY=:0.0\n\n# Start in ~\ncd $HOME" >> $HOME/.bashrc
+echo -e "\n## WSL\n# Forward opengl calls to Windows.\nexport LIBGL_ALWAYS_INDIRECT=1\n# Forward X11 GUIS to Windows.\nexport DISPLAY=:0.0" >> $HOME/.bashrc
